abc81d8a05d4fe52ce6f653c22e75629b259b020
legossol <wkdgothf@gmail.com>
2021-03-10 19:24:12 +0900

    articlaboardmaking

--- be/demo/src/main/java/com/example/demo/bbs/domain/Board.java
+++ be/demo/src/main/java/com/example/demo/bbs/domain/Board.java
@@ -1,8 +1,50 @@
 package com.example.demo.bbs.domain;
 
+import java.io.Serializable;
+
 import org.springframework.stereotype.Component;
 
-@Component
-public class Board {
+import lombok.Data;
+
+@Component @Data
+public class Board implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private long boardNo;
+	private String title;
+	private String index;
+	private String writer;
+	private String Date;
+	
+	public long getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(long boardNo) {
+		this.boardNo = boardNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getIndex() {
+		return index;
+	}
+	public void setIndex(String index) {
+		this.index = index;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getDate() {
+		return Date;
+	}
+	public void setDate(String date) {
+		Date = date;
+	}
+	
 
 }
--- be/demo/src/main/java/com/example/demo/uss/domain/User.java
+++ be/demo/src/main/java/com/example/demo/uss/domain/User.java
@@ -1,11 +1,14 @@
 package com.example.demo.uss.domain;
 
+import java.io.Serializable;
+
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
 @Component @Data
-public class User {
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
 	private long userNo;
 	private String userid;
 	private String passwd;
