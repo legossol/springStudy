65ddde93a6db2cb8cf54ca7fb7ac905fe42e9466
legossol <wkdgothf@gmail.com>
2021-03-16 14:33:03 +0900

    change folder

--- be/demo/src/main/java/com/example/demo/sec/controller/LoginControllerImpl.java
+++ be/demo/src/main/java/com/example/demo/sec/controller/LoginControllerImpl.java
@@ -1,5 +1,11 @@
 package com.example.demo.sec.controller;
 
-public class LoginControllerImpl {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/auth")
+public class LoginControllerImpl {
+	@GetMapping("/login");
 }
