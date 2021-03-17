1a294172a170532b0e76fc2b53ad8ad6fdd59080
legossol <wkdgothf@gmail.com>
2021-03-17 10:03:22 +0900

    rearrange

--- be/demo/src/main/java/com/example/demo/uss/service/UserService.java
+++ be/demo/src/main/java/com/example/demo/uss/service/UserService.java
@@ -1,5 +1,13 @@
 package com.example.demo.uss.service;
+package java.util.
 
 public interface UserService  {
-
+	public Optional<User> findOne(User t)
+	public long count();
+	boolean existsById(long id);
+	public List<Users> findAll(Sort sort);
+	public Optional<Users> findOne();
+	public void deleteById(long id)
+	public User getOnt(long id);
+	public User save(Users entity);
 }
--- be/demo/src/main/java/com/example/demo/uss/service/UserServiceImpl.java
+++ be/demo/src/main/java/com/example/demo/uss/service/UserServiceImpl.java
@@ -4,4 +4,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
+	@Autowired UserRepository repo;
+	void test() {
+		
+	}
 }
