86385847eefa2e1c79ed4bd108984583d4788275
legossol <wkdgothf@gmail.com>
2021-04-15 22:35:41 +0900

    page making

--- mono/reactjs/src/index.js
+++ mono/reactjs/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from'./common/component/Home';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <Home />,
+  
   document.getElementById('root')
 );
 
