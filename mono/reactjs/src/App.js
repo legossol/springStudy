08e028678835b262ddd9195d30cab197cc817a26
legossol <wkdgothf@gmail.com>
2021-04-20 10:00:33 +0900

    redux

--- mono/reactjs/src/App.js
+++ mono/reactjs/src/App.js
@@ -3,20 +3,24 @@ import {ArticleList, ArticleWrite, ArticleDetail, ArticleUpdate} from 'article/i
 import {Login,SignUp,UserDetail} from 'user/index'
 import {News,SeoulCCTV} from 'board/index'
 import { Route } from 'react-router-dom'
-import address
+import { SeoulCCTV, News, Counter, ReduxCounter  } from 'board/index'
+
 const App = () => {
   return (
     <div className="App">
       <Route exact path='/' component={ Home } />
+      
       <Route exact path='/article/article-list' component={ ArticleList } />
       <Route exact path='/article/article-write' component={ ArticleWrite } />
       <Route exact path='/article/article-detail' component={ ArticleDetail } />
       <Route exact path='/article/article-update' component={ ArticleUpdate } />
       <Route exact path='/board/news' component={ News } />
+      <Route exact path='/board/seoul-cctv' component={ SeoulCCTV } />
+      <Route exact path='/board/counter' component={ Counter } />
+      <Route exact path='/board/redux-counter' component={ ReduxCounter } />
       <Route exact path='/user/login' component={Login} />
       <Route exact path='/user/signUp' component={SignUp} />
       <Route exact path='/user/user-detail' component={UserDetail} />
-      <Route exact path='/board/component/seoul-cctv' component={SeoulCCTV}/>
   </div>
   );
 }
--- mono/reactjs/src/board/component/SeoulCCTV.js
+++ mono/reactjs/src/board/component/SeoulCCTV.js
@@ -1,47 +1,46 @@
-import React, { useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import 'common/style/Common.css'
 import 'article/style/ArticleList.css'
-const SeoulCCTV = () => {
-  const[items, setItems] = useState([])//const setItems는 사실 const[items, setItems:setItems()] 뒤에가 제이슨구조 string,"string"같아서
-  
-  const list = () =>{
-  axios.get(`/data/SeoulCCTV.json`)
-  .then((res)=>{
-    setItems(res.data)
-  })//크루저 형식이라 함수안에 함수
-  .catch(err =>{})//크루저 형식이라 함수안에 함수
-}
+import Navi from 'common/component/Navi'
+import Footer from 'common/component/Footer'
+import axios from 'axios'
 
-  const a = () => {return ''}
+const SeoulCCTV = () => {
+  const [items, setItems] = useState([])
+
+  useEffect(()=>{
+    axios.get(`/data/SeoulCCTV.json`)
+    .then(res=>{
+      setItems(res.data.DATA)
+    })
+    .catch(err => {})
+  }, [])
+  
     return (
         <>
-<h1>seoul CCTV</h1>
+<Navi/>
 <table>
   <tr>
-    <th>No.</th>
-    <th>기준날짜</th>
-    <th>카메라코드</th>
-    <th>카메라명칭</th>
-    <th>설명</th>
-    <th>상세</th>
+    <th style={{width: '5%'}}>No.</th>
+    <th style={{width: '15%'}}>기준날짜</th>
+    <th style={{width: '15%'}}>카메라코드</th>
+    <th style={{width: '30%'}}>카메라명칭</th>
+    <th style={{width: '40%'}}>설명</th>
   
   </tr>
-  (items.map((j,i)=>{
-    return(<tr key{i}>
-      <td>{i+1}</td>
-      <td>{j.deviceid}</td>
-      <td>{j.devicename}</td>
-      <td>{j.description</td>
-      </tr>)
-  }))
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
+{ items.map((j, i)=>{
+  return (<tr key={i}>
+    <td style={{width: '5%'}}>{i+1}</td>
+    <td style={{width: '15%'}}>{j.checktime}</td>
+    <td style={{width: '15%'}}>{j.deviceid}</td>
+    <td style={{width: '30%'}}>{j.devicename}</td>
+    <td style={{width: '40%'}}>{j.description}</td>
+  </tr>)
+})
   
+ } 
 </table>
+<Footer/>
         </>
     )
 }
